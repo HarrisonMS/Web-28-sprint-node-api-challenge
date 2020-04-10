684e61b50e12e6daf0f3c8a4f6b44b00aace1251
Harrison <hmseaborn@gmail.com>
2020-04-10 09:00:57 -0700

    added get projects request
    delayed for 20 min rember / before your routes buddy

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const projectRouter = require('../routes/projects-router');
 server.use(helmet());
 server.use(express.json());
 
-server.use('api/projects', projectRouter)
+server.use('/api/projects', projectRouter)
 
 server.get('/', (req, res) => {
   res.send('server is up and running you may continue')
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -2,4 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Projects = require('../data/helpers/projectModel')
 
+
+router.get("/", (req, res) => {
+  Projects.get().then((projects) => {
+      projects.map((project) => {
+          if (project.completed === 0) {
+              project.completed = false;
+          }   else {
+              project.completed = true;
+          }
+      });
+      res.status(200).json(projects); 
+  })
+  .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errrorMessage: "Failed to get projects from data base" }); 
+  });
+});
+
+
 module.exports = router;
