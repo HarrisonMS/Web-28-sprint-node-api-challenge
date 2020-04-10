1d45cddef0a80b3682fea24a2b70950314b3f446
Harrison <hmseaborn@gmail.com>
2020-04-10 08:40:59 -0700

    added project router folder
    required and used in server file

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
+const projectRouter = require('../routes/projects-router');
 
 server.use(helmet());
 server.use(express.json());
 
-
+server.use('api/projects', projectRouter)
 
 server.get('/', (req, res) => {
   res.send('server is up and running you may continue')
