9ae2a2a50ba5849da93913058b95f985b6d2078c
Harrison <hmseaborn@gmail.com>
2020-04-10 09:41:08 -0700

    get projects and actions by project
    lots of oof for an extra .id witht this one

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
+const{ logger } = require('../data/helpers/logger')
 const projectRouter = require('../routes/projects-router');
 
 server.use(helmet());
+server.use(logger)
 server.use(express.json());
 
 server.use('/api/projects', projectRouter)
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -1,29 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const Projects = require('../data/helpers/projectModel')
+const Actions = require('../data/helpers/actionModel')
 
 router.get('/', (req, res) => {
   Projects.get().then((projects) => {
     res.status(200).json(projects);
   })
 })
-// router.get("/", (req, res) => {
-//   Projects.get().then((projects) => {
-//       projects.map((project) => {
-//           if (project.completed === 0) {
-//               project.completed = false;
-//           }   else {
-//               project.completed = true;
-//           }
-//       });
-//       res.status(200).json(projects); 
-//   })
-//   .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({ errrorMessage: "Failed to get projects from data base" }); 
-//   });
-// });
 
+router.get('/:id/actions', (req, res) => {
+  const { id } = req.params;
+  Projects.get(id).then(project => {
+    project ? 
+    res.status(200).send(project.actions) : 
+    res.status(404).send(`could not find any actions for project with id of ${id}`)
+  })
+  .catch((error) => {
+    res.status(500).json(error)
+  })
+  
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  Projects.get(id)
+  .then( project => {
+    project ? 
+    res.status(200).send(project) : 
+    res.status(404).send(`cant find project at id of ${id}`)
+      
+  })
+  .catch(error => {
+
+      res.status(500).json({errorMessage: "error", error});
+  });
+});
 
 
 module.exports = router;
