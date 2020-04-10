511feb642e11a38c99951b22d161e661e38bb290
Harrison <hmseaborn@gmail.com>
2020-04-10 09:07:39 -0700

    hotfix
    get request wasnt functioning correctly

--- routes/projects-router.js
+++ routes/projects-router.js
@@ -2,21 +2,35 @@ const express = require('express');
 const router = express.Router();
 const Projects = require('../data/helpers/projectModel')
 
-
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   Projects.get().then((projects) => {
-      projects.map((project) => {
-          if (project.completed === 0) {
-              project.completed = false;
-          }   else {
-              project.completed = true;
-          }
-      });
-      res.status(200).json(projects); 
+    res.status(200).json(projects);
+  })
+})
+// router.get("/", (req, res) => {
+//   Projects.get().then((projects) => {
+//       projects.map((project) => {
+//           if (project.completed === 0) {
+//               project.completed = false;
+//           }   else {
+//               project.completed = true;
+//           }
+//       });
+//       res.status(200).json(projects); 
+//   })
+//   .catch((err) => {
+//       console.log(err);
+//       res.status(500).json({ errrorMessage: "Failed to get projects from data base" }); 
+//   });
+// });
+
+router.get("/resources", (req, res) => {
+  Projects.get().then((resources) => {
+      res.status(200).json(resources)
   })
   .catch((err) => {
       console.log(err);
-      res.status(500).json({ errrorMessage: "Failed to get projects from data base" }); 
+      res.status.apply(500).json({ errorMessage: "failed to get a list of resources from the data base" })
   });
 });
 
